a35a558fa68848ae042f4836ff24b913ecfbd4d8
FameEdc <ribeiro.edwin@gmail.com>
2016-12-02 10:42:09 +0100

    place xml update

---
+++ app/src/main/java/fr/wildcodeschool/apprenti/sportihome/PlaceActivity.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.apprenti.sportihome;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlaceActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place);
+    }
+}
