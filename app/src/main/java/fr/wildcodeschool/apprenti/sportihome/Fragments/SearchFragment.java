d45c783000ebc94d22b96bf4acf80aa539de0f5a
Chantome Benjamin <benjamin.chantome@gmail.com>
2017-01-18 17:46:42 +0100

    add select list sport with checkbox

--- app/src/main/java/fr/wildcodeschool/apprenti/sportihome/Fragments/SearchFragment.java
+++ app/src/main/java/fr/wildcodeschool/apprenti/sportihome/Fragments/SearchFragment.java
@@ -1,26 +1,42 @@
 package fr.wildcodeschool.apprenti.sportihome.Fragments;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.os.Bundle;
 
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
 
 import fr.wildcodeschool.apprenti.sportihome.Activities.LogInActivity;
+import fr.wildcodeschool.apprenti.sportihome.Adapters.ListSportsAdapter;
 import fr.wildcodeschool.apprenti.sportihome.Model.LogInModel;
 import fr.wildcodeschool.apprenti.sportihome.R;
+import fr.wildcodeschool.apprenti.sportihome.SportNames;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by edwin on 05/12/16.
@@ -39,6 +55,12 @@ public class SearchFragment extends Fragment {
             R.drawable.ic_tabspot
     };
 
+    ListView list;
+    ListSportsAdapter adapter;
+    SearchView editsearch;
+    String[] sportsNameList;
+    ArrayList<SportNames> arraylist = new ArrayList<SportNames>();
+
     public SearchFragment() {}
 
     @Nullable
@@ -48,6 +70,57 @@ public class SearchFragment extends Fragment {
         mLogIn = (LogInModel) getArguments().getSerializable("login");
 
         mView = inflater.inflate(R.layout.fragment_search, null);
+
+        // Generate sample data
+
+        sportsNameList = getResources().getStringArray(R.array.query_suggestions);
+
+        // Locate the ListView in listview_main.xml
+        list = (ListView) mView.findViewById(R.id.listview);
+
+        for (int i = 0; i < sportsNameList.length; i++) {
+            SportNames animalNames = new SportNames(sportsNameList[i]);
+            // Binds all strings into an array
+            arraylist.add(animalNames);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter = new ListSportsAdapter(getActivity(), arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+        editsearch = (SearchView) mView.findViewById(R.id.search);
+
+        editsearch.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                list.setVisibility(View.VISIBLE);
+            }
+        });
+
+        editsearch.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                list.setVisibility(View.GONE);
+                return false;
+            }
+        });
+
+        editsearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                String text = newText;
+                adapter.filter(text);
+                return false;
+            }
+        });
 
         tabLayout = (TabLayout) mView.findViewById(R.id.tabs);
         mViewPager = (ViewPager) mView.findViewById(R.id.viewpager);
@@ -72,7 +145,6 @@ public class SearchFragment extends Fragment {
         tabLayout.getTabAt(1).setIcon(tabIcons[1]);
         tabLayout.getTabAt(getArguments().getInt("tab")).select();
     }
-
 
     class MyAdapter extends FragmentPagerAdapter {
 
